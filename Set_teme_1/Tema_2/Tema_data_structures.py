5bf184eacf880819e3cfa578f0db4afc2c164a85
unknown <neacsugabriela93@gmail.com>
2024-09-22 23:19:56 +0300

    incomplet (nu inteleg cerinta)

---
+++ Set_teme_1/Tema_2/Tema_data_structures.py
@@ -0,0 +1,13 @@
+lista = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+lista_asc = lista
+
+for i in range(len(lista_asc)-1):
+    for j in range(len(lista_asc)-i-1):
+        if lista_asc[j] > lista_asc[j+1]:
+            lista_asc[j], lista_asc[j+1] = lista_asc[j+1], lista_asc[j]
+print(f'Lista ascendenta este: {lista_asc}')
+print(f'Lista descendenta este: {lista_asc[::-1]}')
+
+
+
+
